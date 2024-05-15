f1c74ea33b6fead6a93d8d37b72dd458b42748c7
csangharsha <sangharsha.chaulagain@gmail.com>
2024-05-15 12:27:58 +0545

    config

--- all.sh
+++ all.sh
@@ -2,5 +2,5 @@
 DEPLOYUSERNAME="csangharsha"
 PASSWORD="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"
 echo "$PASSWORD" | docker login https://ghcr.io -u $DEPLOYUSERNAME --password-stdin
-docker-compose -f docker-compose.yml up -d
+docker compose -f docker-compose.yml up -d
 docker logout ghcr.io
